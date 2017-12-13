dc1f91c4e4ef8705620eb0578fac69aff2bd629d
khuram26 <khurams2626@gmail.com>
2017-12-13 13:19:11 +0500

    initial setup

---
+++ src/TimezonesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Laraveldaily\Timezones;
+ 
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+ 
+class TimezonesController extends Controller
+{
+ 
+    // public function index($timezone)
+    // {
+    //     echo Carbon::now($timezone)->toDateTimeString();
+    // }
+ 
+ 	public function index($timezone = NULL)
+    {
+    	$current_time = ($timezone)
+            ? Carbon::now(str_replace('-', '/', $timezone))
+            : Carbon::now();
+        return view('timezones::time', compact('current_time'));
+        // $time = ($timezone)
+        //     ? Carbon::now(str_replace('-', '/', $timezone))
+        //     : Carbon::now();
+        // echo $time->toDateTimeString();
+    }
+}
